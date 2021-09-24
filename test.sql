f50a2ab2ff2836a11bfe88c19097e65177284552
ilyanazalun <iyang_agung_s@hotmail.com>
2021-09-24 18:53:59 +0700

    first commit

---
+++ test.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 24, 2021 at 01:45 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`` PROCEDURE `AddGeometryColumn` (`catalog` VARCHAR(64), `t_schema` VARCHAR(64), `t_name` VARCHAR(64), `geometry_column` VARCHAR(64), `t_srid` INT)  begin
+  set @qwe= concat('ALTER TABLE ', t_schema, '.', t_name, ' ADD ', geometry_column,' GEOMETRY REF_SYSTEM_ID=', t_srid); PREPARE ls from @qwe; execute ls; deallocate prepare ls; end$$
+
+CREATE DEFINER=`` PROCEDURE `DropGeometryColumn` (`catalog` VARCHAR(64), `t_schema` VARCHAR(64), `t_name` VARCHAR(64), `geometry_column` VARCHAR(64))  begin
+  set @qwe= concat('ALTER TABLE ', t_schema, '.', t_name, ' DROP ', geometry_column); PREPARE ls from @qwe; execute ls; deallocate prepare ls; end$$
+
+--
+-- Functions
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `ExtractNumber` (`in_string` VARCHAR(50)) RETURNS INT(11) NO SQL
+BEGIN
+    DECLARE ctrNumber VARCHAR(50);
+    DECLARE finNumber VARCHAR(50) DEFAULT '';
+    DECLARE sChar VARCHAR(1);
+    DECLARE inti INTEGER DEFAULT 1;
+    IF LENGTH(in_string) > 0 THEN
+        WHILE(inti <= LENGTH(in_string)) DO
+            SET sChar = SUBSTRING(in_string, inti, 1);
+            SET ctrNumber = FIND_IN_SET(sChar, '0,1,2,3,4,5,6,7,8,9'); 
+            IF ctrNumber > 0 THEN
+                SET finNumber = CONCAT(finNumber, sChar);
+            END IF;
+            SET inti = inti + 1;
+        END WHILE;
+        RETURN CAST(finNumber AS UNSIGNED);
+    ELSE
+        RETURN 0;
+    END IF;    
+END$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `fromRoman` (`inRoman` VARCHAR(15)) RETURNS INT(11) BEGIN
+
+    DECLARE numeral CHAR(7) DEFAULT 'IVXLCDM';
+
+    DECLARE digit TINYINT;
+    DECLARE previous INT DEFAULT 0;
+    DECLARE current INT;
+    DECLARE sum INT DEFAULT 0;
+
+    SET inRoman = UPPER(inRoman);
+
+    WHILE LENGTH(inRoman) > 0 DO
+        SET digit := LOCATE(RIGHT(inRoman, 1), numeral) - 1;
+        SET current := POW(10, FLOOR(digit / 2)) * POW(5, MOD(digit, 2));
+        SET sum := sum + POW(-1, current < previous) * current;
+        SET previous := current;
+        SET inRoman = LEFT(inRoman, LENGTH(inRoman) - 1);
+    END WHILE;
+
+    RETURN sum;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_answers`
+--
+
+CREATE TABLE `tbl_answers` (
+  `id` varchar(25) NOT NULL,
+  `question_id` varchar(25) NOT NULL,
+  `answer` text NOT NULL,
+  `created_at` date NOT NULL,
+  `updated_at` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_answers`
+--
+
+INSERT INTO `tbl_answers` (`id`, `question_id`, `answer`, `created_at`, `updated_at`) VALUES
+('99444449813200897', '99444449813200896', 'Baik', '2021-09-24', '2021-09-24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_items`
+--
+
+CREATE TABLE `tbl_items` (
+  `id` int(25) NOT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  `value` int(25) DEFAULT NULL,
+  `date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_items`
+--
+
+INSERT INTO `tbl_items` (`id`, `name`, `value`, `date`) VALUES
+(1, 'PHP', 1, '2021-09-24'),
+(2, 'Java', 1, '2021-09-24'),
+(3, 'Python', 1, '2021-09-24'),
+(4, 'Asembly', 1, '2021-09-24'),
+(5, 'Ruby', 1, '2021-09-24'),
+(6, 'PHP', 1, '2021-09-24'),
+(7, 'Java', 1, '2021-09-24'),
+(8, 'Python', 1, '2021-09-24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_name`
+--
+
+CREATE TABLE `tbl_name` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_questions`
+--
+
+CREATE TABLE `tbl_questions` (
+  `id` varchar(25) NOT NULL,
+  `question` text NOT NULL,
+  `created_at` date NOT NULL,
+  `updated_at` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_questions`
+--
+
+INSERT INTO `tbl_questions` (`id`, `question`, `created_at`, `updated_at`) VALUES
+('99444449813200896', 'Hello Semua ?', '2021-09-24', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_items`
+--
+ALTER TABLE `tbl_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_name`
+--
+ALTER TABLE `tbl_name`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_items`
+--
+ALTER TABLE `tbl_items`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tbl_name`
+--
+ALTER TABLE `tbl_name`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
